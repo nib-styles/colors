6a28c9d3df5a98d82c42356d77702ec75b21b7dd
Laurie Jones <hello@lauriejones.me>
2017-03-07 11:19:11 +1100

    Add js file for styled-components

---
+++ index.js
@@ -0,0 +1,28 @@
+export default const colors = {
+
+  //greens
+  'elizabeth': '#009623',
+  'charles': '#0a6d12',
+  'harry': '#26b11b',
+
+  //orange
+  'kate': '#ffb400',
+
+  //DEPRECATED: Use single word names.
+  //Colors now used in js. Want to be able to use colors.color, not colors['color-name']
+  'queen-elizabeth': '#009623',
+  'prince-charles': '#0a6d12',
+  'prince-harry': '#26b11b',
+  'princess-kate': '#ffb400',
+
+  //greys
+  white: '#ffffff',
+  sneezy: '#eeeeee',
+  sleepy: '#dddddd',
+  happy: '#cccccc',
+  grumpy: '#888888',
+  dopey: '#666666',
+  doc: '#444444',
+  bashful: '#222222'
+
+};
